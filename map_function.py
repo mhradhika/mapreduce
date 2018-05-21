d6dd1bb77831d0a3054d1cd6119b55b0a0f36ea2
mhradhika <mhradhika@gmail.com>
2018-05-21 09:09:29 +0530

    Create map_function.py

---
+++ map_function.py
@@ -0,0 +1,17 @@
+import pymongo
+from pymongo import *
+Connection = MongoClient('mongodb://localhost:27017/')
+from bson.code import Code
+from bson.son import SON
+db = Connection.test
+
+#Creating Map function for notification collection*/
+map_function = Code("""
+  function(){
+  if(this.year == null) return;
+
+  var key = {
+       cancer_site:this.cancer_site };
+  emit(key, {count:NumberInt(1)});
+  }
+""")
